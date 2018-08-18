ccd5175e343f51fa88fa370b32367b8254d3a99b
sovannarith_che_kh <sovannarith.che@ascendcorp.com>
2018-08-18 10:20:38 +0700

    TK

--- src/main/java/com/tk/framework/tools/Tk.java
+++ src/main/java/com/tk/framework/tools/Tk.java
@@ -4,8 +4,7 @@ package com.tk.framework.tools;
  * Created by sovannarith on 8/18/18
  */
 public class Tk {
-
-
+    
     public static void print(){
         A.b("---------------Tarang Framework-------------");
     }
